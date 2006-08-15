27b4e321cdc2ad6134787c15ae9e9406ce88a880
ryan <none@none>
2006-08-15 20:21:34 +0000

    continuing the PEP8 roundup. spaces look a lot better in trac than tabs ;)
    --HG--
    branch : fuzzy

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Pyrex.Distutils import build_ext
 
-setup(name='fuzzy',
-	ext_modules=[
-		Extension('fuzzy', ['src/fuzzy.pyx', 'src/double_metaphone.c']),
-	],
-	cmdclass = {'build_ext' : build_ext}
+setup(
+    name='fuzzy',
+    ext_modules=[Extension('fuzzy', ['src/fuzzy.pyx', 'src/double_metaphone.c'])],
+    cmdclass = {'build_ext' : build_ext}
 )
