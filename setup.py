3a981573b2c372f20d9675cc9bb1d1eac2602c80
ryan <none@none>
2006-08-28 22:22:05 +0000

    normalizing egg names
    --HG--
    branch : fuzzy

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 from distutils.extension import Extension
 from Pyrex.Distutils import build_ext
 
 setup(
-    name='fuzzy',
-    ext_modules=[Extension('fuzzy', ['src/fuzzy.pyx', 'src/double_metaphone.c'])],
-    cmdclass = {'build_ext' : build_ext}
+    name        = 'Polimetrix-Fuzzy',
+    version     = '1.0',
+    ext_modules = [Extension('fuzzy', ['src/fuzzy.pyx', 'src/double_metaphone.c'])],
+    cmdclass    = {'build_ext' : build_ext}
 )
