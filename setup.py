d14d7de8701dfa04f025d1a4064118d82c80d6c0
Chris Mulligan <chris.mulligan@gmail.com>
2011-03-23 14:23:21 -0400

    Setup.py now uses pyrex when available, but if not uses the included C code.
    --HG--
    branch : fuzzy

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup
 from distutils.extension import Extension
-from Pyrex.Distutils import build_ext
+try:
+    from Pyrex.Distutils import build_ext
+    use_pyrex = True
+except ImportError:
+    use_pyrex = False
+
+cmdclass = {}
+ext_modules = []
+
+if use_pyrex:
+    ext_modules = [Extension('fuzzy', ['src/fuzzy.pyx', 'src/double_metaphone.c'])]
+    cmdclass.update({ 'build_ext': build_ext })
+else:
+    ext_modules = [Extension('fuzzy', ['src/fuzzy.c', 'src/double_metaphone.c'])]
 
 setup(
-    name        = 'Polimetrix-Fuzzy',
+    name        = 'Fuzzy',
     version     = '1.0',
-    ext_modules = [Extension('fuzzy', ['src/fuzzy.pyx', 'src/double_metaphone.c'])],
-    cmdclass    = {'build_ext' : build_ext},
+    ext_modules = ext_modules,
+    cmdclass    = cmdclass,
     zip_safe    = False
 )
