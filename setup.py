fdef30b756f4c7d10a29f3a178c27df0de3d38b5
Jason R. Coombs <jaraco@jaraco.com>
2015-06-04 09:37:45 -0400

    Normalize whitespace
    --HG--
    branch : fuzzy

--- setup.py
+++ setup.py
@@ -16,15 +16,15 @@ else:
     ext_modules = [Extension('fuzzy', ['src/fuzzy.c', 'src/double_metaphone.c'])]
 
 setup(
-    name        = 'Fuzzy',
-    version     = '1.1',
-    ext_modules = ext_modules,
-    cmdclass    = cmdclass,
-    description = "Fast Python phonetic algorithms",
-    maintainer  = "YouGov, Plc.",
-    maintainer_email = "dev@yougov.com",
-    url         = "https://bitbucket.org/yougov/fuzzy",
-    classifiers = [
+    name='Fuzzy',
+    version='1.1',
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
+    description="Fast Python phonetic algorithms",
+    maintainer="YouGov, Plc.",
+    maintainer_email="dev@yougov.com",
+    url="https://bitbucket.org/yougov/fuzzy",
+    classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'License :: OSI Approved :: Artistic License',
@@ -34,7 +34,7 @@ setup(
         'Topic :: Text Processing :: General',
         'Topic :: Text Processing :: Indexing',
         'Topic :: Text Processing :: Linguistic',
-        ],
-    long_description = open('README').read(),
-    zip_safe    = False,
+    ],
+    long_description=open('README').read(),
+    zip_safe=False,
 )
