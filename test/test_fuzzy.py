43633dc25cfcad589430c15edea80fdbc2c24eba
Jason R. Coombs <jaraco@jaraco.com>
2017-06-04 20:47:17 -0400

    Update test so it now passes (and also fails if 14c9b45 is reverted). Did this test work on older Pythons?

--- test/test_fuzzy.py
+++ test/test_fuzzy.py
@@ -6,4 +6,4 @@ def test_soundex_does_not_mutate_strings():
 	phrase = 'FancyFree'
 	fuzzy.Soundex(4)(phrase)
 	buffer = ctypes.c_char_p(phrase)
-	assert str(buffer) == "c_char_p('FancyFree')"
+	assert buffer.value == "FancyFree"
