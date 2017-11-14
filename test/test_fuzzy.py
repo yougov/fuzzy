fa184ba40b7fe2767985a4ec9c90af5e52cbc461
Jason R. Coombs <jason.coombs@yougov.com>
2017-11-14 15:47:28 -0500

    Add test capturing expectation as found in version 1.1. Ref #14.

--- test/test_fuzzy.py
+++ test/test_fuzzy.py
@@ -2,8 +2,15 @@ import ctypes
 
 import fuzzy
 
+
 def test_soundex_does_not_mutate_strings():
 	phrase = 'FancyFree'
 	fuzzy.Soundex(4)(phrase)
 	buffer = ctypes.c_char_p(phrase)
 	assert str(buffer) == "c_char_p('FancyFree')"
+
+
+def test_soundex_result():
+	phrase = 'FancyFree'
+	res = fuzzy.Soundex(4)(phrase)
+	assert res == 'F521'
